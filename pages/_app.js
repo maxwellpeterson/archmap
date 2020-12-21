a44a8b13cf5be81f87346c3806950f152dbb0788
Max Peterson <maxpeterson00@gmail.com>
2020-12-20 17:42:26 -0800

    hacky map setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+// Taken from github.com/vercel/next.js/blob/canary/examples/with-styled-components/pages/_app.js
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { createGlobalStyle, ThemeProvider } from "styled-components"
+import { normalize } from "styled-normalize"
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
+  * {
+    box-sizing: border-box;
+  }
+`
+
+// Eventually do stuff with themes...
+const theme = {
+  colors: {
+    primary: "#0070f3",
+  },
 }
+
+const MyApp = ({ Component, pageProps }) => (
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
+)
 
 export default MyApp
