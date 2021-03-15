3ea69fa98b8f902cf963c4c71e04a7df0be89bbd
Max Peterson <maxpeterson00@gmail.com>
2021-03-14 21:30:46 -0700

    Created a test stack for creating Lambdas from ECR images.

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,9 +1,38 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as dynamodb from "@aws-cdk/aws-dynamodb";
+import * as ecr from "@aws-cdk/aws-ecr";
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // const archDailyProjects = new dynamodb.Table(this, "ArchDailyProjects", {
+    //   partitionKey: { name: "id", type: dynamodb.AttributeType.NUMBER },
+    // });
+
+    // const archDailyScraper = new lambda.Function(this, "ArchDailyScraper", {
+    //   runtime: lambda.Runtime.NODEJS_14_X,
+    //   handler: "archdaily-api.handler",
+    //   code: lambda.Code.fromCfnParameters(),
+    //   environment: {
+    //     ARCHDAILY_TABLE_NAME: archDailyProjects.tableName,
+    //   },
+    // });
+
+    const testFunctionImage = ecr.Repository.fromRepositoryName(
+      this,
+      "TestFunctionImage",
+      "archmap-scrapers"
+    );
+
+    const testFunction = new lambda.Function(this, "TestFunction", {
+      code: lambda.Code.fromEcrImage(testFunctionImage),
+      handler: lambda.Handler.FROM_IMAGE,
+      runtime: lambda.Runtime.FROM_IMAGE,
+      environment: {
+        TEST_MESSAGE: "Oopity",
+      },
+    });
   }
 }
